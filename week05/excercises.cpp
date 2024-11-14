65cacb9ce1ad92a4898bd0572e9bc65d050d165f
František Matušinec <frantisek.matusinec@email.cz>
2024-11-14 15:53:07 +0100

    Done v1

--- week05/excercises.cpp
+++ week05/excercises.cpp
@@ -1,20 +1,70 @@
+#include <iostream>
 // TODO: Doplňte potřebné hlavičky
 
 // Vypište obdélník o zadaných parametrech
 // Vraťte 0 pokud vše v pořádku, -1 pokud nastala chyba
 int hollow_rect(int x, int y, int thickness) {
+    int mezera = y-(2*thickness);
+    int mezera2 = x-(2*thickness);
+    for(int i = 0; i < thickness; i++){
+        for(int i = 0; i < x; i++){
+            std::cout << "*";
+        }
+        std::cout << std::endl;
+    }
+    for(int i = 0; i < mezera; i++){
+        for(int i = 0; i < thickness; i++){
+            std::cout << "*";
+        }
+        for(int i = 0; i < mezera2; i++){
+            std::cout << " ";
+        }
+        for(int i = 0; i < thickness; i++){
+            std::cout << "*";
+        }
+        std::cout << std::endl;
+    }
+    for(int i = 0; i < thickness; i++){
+        for(int i = 0;i < x; i++){
+            std::cout << "*";
+        }
+        std::cout << std::endl;
+    }
     return -1;
 }
 
 // Vraťte a na n
 int power(int a, int n) {
+    int cislo = 1;
+    for(int i = 0; i < n; i++){
+        cislo = cislo*a;
+    }
+    std::cout << cislo << std::endl;
     return 0;
 }
 
 // Vraťte idx-té fibonacciho číslo
 int fibonacci(int idx) {
+    int vysledek2 = 0;
+    int vysledek = 1;
+    int indx = (idx/2);
+    for(int i = 1; i < indx; i++){
+        vysledek2 += vysledek;
+        vysledek += vysledek2;  
+    }
+    if(idx % 2 == 0){
+        std::cout << vysledek2 << std::endl;
+    }
+    else{
+        std::cout << vysledek << std::endl;
+    }
     return 0;
 }
 
 int main() {
+
+//hollow_rect(20, 20, 4);
+//power(4, 3);
+fibonacci(7);
+
 }
--- week06/hw.cpp
+++ week06/hw.cpp
@@ -1,7 +1,37 @@
 #include <cassert>
+#include<iostream>
 // TODO: doplťe co potřebujete
 
-
+int power_digit_sum(unsigned int number){
+    int pocet_cifer = 0;
+    int podil;
+    int cislo = number;
+    while(podil > 0){//zjisti pocet cifer pro vypocet prvni a nasledujicich mocnin
+        podil = number/7;
+        number  = podil;
+        pocet_cifer += 1;
+    }
+    int sum =0;
+    int mocnina = pocet_cifer;
+    
+    for(int i = 0; i < pocet_cifer; i++){//provadi soucet mocnin na cifrach
+        int cislo_cifra = cislo % 7;
+        int podil2 = cislo / 7;
+        cislo = podil2;
+        int cifra_umocnena = 1;
+        for(int i = 0; i < mocnina; i++){//umocnuje cislo na cifre
+            cifra_umocnena *= cislo_cifra;
+            std::cout << "cislo na cifre je ";
+            std::cout << cislo_cifra <<std::endl;
+        }
+        mocnina -= 1;
+        sum += cifra_umocnena;
+        std::cout << cifra_umocnena << std::endl;
+    }
+    
+    std::cout<< pocet_cifer << std::endl;
+    return sum;
+}
 // Implementujte funkci ‹power_digit_sum›, která vrátí „speciální“
 // ciferný součet čísla ‹number›, který se od běžného ciferného
 // součtu liší tím, že každou cifru před přičtením umocníme na číslo
@@ -22,8 +52,24 @@
 // platí ⟦f(x) ≥ f(x')⟧, kde ⟦f⟧ je funkce, kterou počítá podprogram
 // ‹poly›).
 
-int poly(int x) {
-    return 10 + 30 * x - 15 * x * x * x + x * x * x * x * x;
+int poly(int low, int high) {
+    int x = low;
+    int diference = high - low;
+    int position;
+    int value = 10 + 30 * x - 15 * x * x * x + x * x * x * x * x;
+    int max_value = value;
+    for(int i = 0; i <= diference; i++){
+        value = 10 + 30 * x - 15 * x * x * x + x * x * x * x * x;
+        if(max_value < value){
+            max_value = value;
+            position = x;
+        }
+        x += 1;
+    }
+    std::cout<<"Hledané číslo x je: ";
+    std::cout<< position << std::endl;
+    std::cout << "Hodnota je: ";
+    return max_value;
 }
 
 
@@ -59,4 +105,57 @@ int first_day(int year) {
     int years = year - 1601;
     int offset = years + years / 4 - years / 100 + years / 400;
     return offset % 7;
+}
+int work_days(int year){
+    int prvni_den = first_day(year);
+    int pocet_prac_dni = 365;
+    bool prestupny_rok = false;
+    if(year % 4 == 0){
+        pocet_prac_dni = 366;
+        prestupny_rok = true;
+    }
+    pocet_prac_dni -= 2;//odecte 2 dny velikonoc
+    if(prestupny_rok == false){//normalní rok
+        pocet_prac_dni -= 104;
+        if(prvni_den == 5 || prvni_den == 6) pocet_prac_dni-= 1;
+        if(prvni_den != 5 && prvni_den != 6)pocet_prac_dni -= 1;//novy rok
+        if(121 % 7 + prvni_den -1 != 5 && 121 % 7 + prvni_den -1 != 6 && 122 % 7 + prvni_den -1 != -1)pocet_prac_dni -= 1;//svatek prace
+        if(128 % 7 + prvni_den -1 != 5 && 128 % 7 + prvni_den -1 != 6 && 128 % 7 + prvni_den -1 != -1)pocet_prac_dni -= 1;//den vitezstvi
+        if(186 % 7 + prvni_den -1 != 5 && 186 % 7 + prvni_den -1 != 6 && 186 % 7 + prvni_den -1 != -1)pocet_prac_dni -= 1;//cyril a metodej
+        if(187 % 7 + prvni_den -1 != 5 && 187 % 7 + prvni_den -1 != 6 && 187 % 7 + prvni_den -1 != -1)pocet_prac_dni -= 1;//jan hus
+        if(271 % 7 + prvni_den -1 != 5 && 271 % 7 + prvni_den -1 != 6 && 271 % 7 + prvni_den -1 != -1)pocet_prac_dni -= 1;//vaclav
+        if(301 % 7 + prvni_den -1 != 5 && 301 % 7 + prvni_den -1 != 6 && 301 % 7 + prvni_den -1 != -1)pocet_prac_dni -= 1;//den vzniku csr
+        if(321 % 7 + prvni_den -1 != 5 && 321 % 7 + prvni_den -1 != 6 && 321 % 7 + prvni_den -1 != -1)pocet_prac_dni -= 1;//den boje za svobodu a demokracii
+        if(358 % 7 + prvni_den -1 != 5 && 358 % 7 + prvni_den -1 != 6 && 358 % 7 + prvni_den -1 != -1)pocet_prac_dni -= 1;//stedry den
+        if(359 % 7 + prvni_den -1 != 5 && 359 % 7 + prvni_den -1 != 6 && 359 % 7 + prvni_den -1 != -1)pocet_prac_dni -= 1;//1 svatek vanocni
+        if(360 % 7 + prvni_den -1 != 5 && 360 % 7 + prvni_den -1 != 6 && 360 % 7 + prvni_den -1 != -1)pocet_prac_dni -= 1;//2 svatek vanocni
+    }
+    else{//prestupny rok
+        pocet_prac_dni -= 104;
+        if(prvni_den == 5){
+            pocet_prac_dni -= 2;
+        }
+        if(prvni_den == 6){
+            pocet_prac_dni -= 1;
+        }
+        if(prvni_den != 5 && prvni_den != 6)pocet_prac_dni -= 1;//novy rok-1
+        if(122 % 7 + prvni_den -1 != 5 && 122 % 7 + prvni_den -1 != 6 && 122 % 7 + prvni_den -1 != -1)pocet_prac_dni -= 1;//svatek prace-1
+        if(129 % 7 + prvni_den -1 != 5 && 129 % 7 + prvni_den -1 != 6 && 129 % 7 + prvni_den -1 != -1)pocet_prac_dni -= 1;//den vitezstvi-1
+        if(187 % 7 + prvni_den -1 != 5 && 187 % 7 + prvni_den -1 != 6 && 187 % 7 + prvni_den -1 != -1)pocet_prac_dni -= 1;//cyril a metodej-1
+        if(188 % 7 + prvni_den -1 != 5 && 188 % 7 + prvni_den -1 != 6 && 188 % 7 + prvni_den -1 != -1)pocet_prac_dni -= 1;//jan hus 0
+        if(272 % 7 + prvni_den -1 != 5 && 272 % 7 + prvni_den -1 != 6 && 272 % 7 + prvni_den -1 != -1)pocet_prac_dni -= 1;//vaclav 0
+        if(302 % 7 + prvni_den -1 != 5 && 302 % 7 + prvni_den -1 != 6 && 302 % 7 + prvni_den -1 != -1)pocet_prac_dni -= 1;//den vzniku csr-1
+        if(322 % 7 + prvni_den -1 != 5 && 322 % 7 + prvni_den -1 != 6 && 322 % 7 + prvni_den -1 != -1)pocet_prac_dni -= 1;//den boje za svobodu a demokracii 0
+        if(359 % 7 + prvni_den -1 != 5 && 359 % 7 + prvni_den -1 != 6 && 359 % 7 + prvni_den -1 != -1)pocet_prac_dni -= 1;//stedry den -1
+        if(360 % 7 + prvni_den -1 != 5 && 360 % 7 + prvni_den -1 != 6 && 360 % 7 + prvni_den -1 != -1)pocet_prac_dni -= 1;//1 svatek vanocni -1
+        if(361 % 7 + prvni_den -1 != 5 && 361 % 7 + prvni_den -1 != 6 && 361 % 7 + prvni_den -1 != -1)pocet_prac_dni -= 1;//2 svatek vanocni -1
+    }
+
+    return pocet_prac_dni;
+}
+int main(){
+    std::cout << work_days(2024) << std::endl;
+    //std::cout << power_digit_sum(353) <<std::endl;
+    //std::cout<< poly(5, 15) << std::endl;
+
 }
