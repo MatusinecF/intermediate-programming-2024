0b3676be77bc0043239a0da3bf204a42a6eb6270
František Matušinec <frantisek.matusinec@email.cz>
2025-02-06 19:16:10 +0100

    dochazka zamestnancu

--- week13/overloading.cpp
+++ week13/overloading.cpp
@@ -3,33 +3,49 @@
 #include <vector>
 
 int sum(int a, int b) {
-    return 0;
-    
+    return a + b;
 }
 
 std::string sum(std::string a, std::string b) {
-    return "";
+    std::string vysledek;
+    
+    return a+b;
 }
 
 int sum(std::vector<int> xs) {
-    return 0;
+    int vysledek = 0;
+    for(size_t i = 0; i < xs.size(); i++){
+        vysledek += xs[i];
+    }
+    return vysledek;
 }
 
 std::string sum(std::vector<std::string> xs) {
-    return "";
+    std::string vysledek;
+    for(size_t i = 0; i < xs.size(); i++){
+        vysledek += xs[i];
+    }
+    return vysledek;
 }
 
 int mul(int a, int b) {
-    return 0;
+    return a * b;
 }
 
 std::string mul(int a, std::string b) {
-    return "";
+    std::string vysledek;
+    for(int i = 0; i < a; i++){
+        vysledek += b;
+    }
+    return vysledek;
 }
+
+
+
 
 int main() {
     std::cout << "sum(3, 4): " << sum(3, 4) << "\n"; // 7
-    std::cout << R"(sum("Hello, ", "World!"): )" << sum("Hello, ", "World!") << "\n"; // Hello World
+    std::cout << R"(sum("Hello, ", "World!"): )" << sum("Hello, ", "World!") << "\n"; // Hello, World!
     std::cout << "sum({1, 2, 3, 4, 5}): " << sum(std::vector<int>{1, 2, 3, 4, 5}) << "\n"; // 15
     std::cout << R"(sum({"abc", "def", "ghi"}): )" << sum(std::vector<std::string>{"abc", "def", "ghi"}) << "\n"; // abcdefghi
 
